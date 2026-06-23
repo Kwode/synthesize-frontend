11e7a0188b797f93b63c58c22ffb71451ebdb2cd
Kwode <obruchekwode@gmail.com>
2026-06-23 17:21:36 +0100

    added backend url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://synthesize-backend.onrender.com",
 });
 
 // Request interceptor to attach bearer token
